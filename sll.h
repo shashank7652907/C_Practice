2f268372f914bc9f20a3f1752875bcd2529aa6fb
shashank <shashankadepu@gmail.com>
2025-09-25 00:24:47 +0530

    first commit

---
+++ sll.h
@@ -0,0 +1,13 @@
+#ifndef SLL_H
+#define SLL_H
+
+
+int init(int data);
+int add(int data);
+int add_at_end(int data);
+int exists(int data);
+int delete(int data);
+int insert(int key, int data);
+void traverse(void);
+
+#endif
